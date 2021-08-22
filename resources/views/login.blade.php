ac1df8b1d204a7a9fce858a754c429256c57d0fa
Ji-geon-woo <s19078@gsm.hs.kr>
2021-08-22 23:41:14 +0900

    ch01

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,31 @@
 @extends('master.main')
-@section('title','Home')
+@section('title','Login')
 @section('content')
-
-
+@push('styles')
+    <link href="{{ asset('resources/css/login.css') }}" rel="stylesheet">
+@endpush
+    <div class="loginbox">
+        <div class="logininner">
+            <div class="logininput">
+                <div class="email">
+                    <p>이메일</p>
+                    <input type="email">
+                </div>
+                <div class="password">
+                    <p>비밀번호</p>
+                    <input type="password">
+                </div>
+            </div>
+            <div class="loginbutton">
+                <input type="button" value="로그인">
+            </div>
+            <div class="emailremeber">
+                <input type="checkbox" name="" id="">
+                <p>이메일 기억하기</p>
+            </div>
+            <div class="singup">
+                <a href="/singup">관리자 등록하기</a>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/master/main.blade.php
+++ resources/views/master/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('resources/css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('resources/css/reset.css') }}" rel="stylesheet">
+    @stack('styles')
     <title>@yield('title')</title>
 </head>
 <body>
